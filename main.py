1ccd13a36897530350f0e608168f0575a9853223
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2026-01-31 16:20:08 +0000

    Adding xpath for the input form

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from selenium.webdriver.common.by import By
 
 url_google_form = 'https://docs.google.com/forms/d/e/1FAIpQLScaD7aQQqC_-SkGJwkaCTHhHIHTnMS-jge86wIFCfd8MWkmfA/viewform'
 url_zilo_clone = "https://appbrewery.github.io/Zillow-Clone/"
-input_address = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input'
-input_price = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input'
-input_link = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input'
+input_address_path = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input'
+input_price_path = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input'
+input_link_path = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input'
 submit_path='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span'
 
 
@@ -33,6 +33,14 @@ def search_flats():
 def fill_form(link_list, price_list, address_list):
     driver = webdriver.chrome()
     driver.get(url_google_form)
+    
+
+    address_input = driver.find_element(By.XPATH, input_address_path)
+    price_input = driver.find_element(By.XPATH, input_price_path)
+    link_input = driver.find_element(By.XPATH, input_link_path)
+    submit = driver.find_element(By.XPATH, submit_path)
+
+    
 
 
 
